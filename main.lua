7a556db4472b353c5b614398cca8df2787726868
Ryan Malonzo <ryan@ryanmalonzo.fr>
2021-08-27 01:09:35 +0200

    Add player attack

--- main.lua
+++ main.lua
@@ -9,10 +9,23 @@ function love.load()
 
     -- World
     world = windfield.newWorld(0, 400, false)
+    world:addCollisionClass("Player")
+    world:addCollisionClass("Boundary")
 
-    -- Ground collider
-    ground = world:newRectangleCollider(0, 456, love.graphics.getWidth(), 40)
-    ground:setType("static")
+    -- Ground
+    Boundary = world:newRectangleCollider(0, 456, love.graphics.getWidth(), 40)
+    Boundary:setType("static")
+    Boundary:setCollisionClass("Boundary")
+
+    -- Walls
+    walls = {
+        wall_1 = world:newRectangleCollider(-1, 0, 1, love.graphics.getHeight()),
+        wall_2 = world:newRectangleCollider(love.graphics.getWidth(), 0, 1, love.graphics.getHeight())
+    }
+    for _, wall in pairs(walls) do
+        wall:setType("static")
+        wall:setCollisionClass("Boundary")
+    end
 
     -- Sprites
     sprites = {
@@ -25,13 +38,17 @@ function love.load()
     p_animations = {
         idle = anim8.newAnimation(player_grid("1-4", 1), 0.2),
         run = anim8.newAnimation(player_grid("2-5", 2), 0.1),
+        atk = anim8.newAnimation(player_grid("1-5", 7), 0.1),
     }
 
-    -- Player entity
+    -- Player
     player = world:newRectangleCollider(0, 425, 25, 30)
+    Boundary:setCollisionClass("Player")
+
     player.direction = 1
     player.speed = 150
     player.animation = p_animations.idle
+    player.attacking = false
     
     -- Camera
     camera = gamera.new(0, 0, love.graphics.getWidth(), love.graphics.getHeight())
@@ -42,17 +59,22 @@ function love.update(dt)
     world:update(dt)
     
     local px, py = player:getPosition()
-    -- Controls
-    if love.keyboard.isDown("q") then
-        player.animation = p_animations.run
-        player.direction = -1
-        player:setX(px - player.speed * dt)
-    elseif love.keyboard.isDown("d") then
-        player.animation = p_animations.run
-        player.direction = 1
-        player:setX(px + player.speed * dt)
-    else
-        player.animation = p_animations.idle
+    if not player.attacking then
+        -- Controls
+        if love.keyboard.isDown("q") then
+            player.animation = p_animations.run
+            player.direction = -1
+            player:setX(px - player.speed * dt)
+        elseif love.keyboard.isDown("d") then
+            player.animation = p_animations.run
+            player.direction = 1
+            player:setX(px + player.speed * dt)
+        else
+            player.animation = p_animations.idle
+        end
+
+    elseif player.animation.position == 5 then
+        player.attacking = false
     end
 
     -- Animations
@@ -64,13 +86,18 @@ end
 
 function love.draw()
     camera:draw(drawCamera)
+end
 
-    -- DEBUG
-    love.graphics.print("Cursor position: " .. love.mouse.getX() .. ", " .. love.mouse.getY(), 0, 0)
+function love.mousepressed(x, y, button, istouch, presses)
+    if button == 1 and not player.attacking then
+        player.attacking = true
+        player.animation = p_animations.atk
+        player.animation:gotoFrame(1)
+    end
 end
 
 function drawCamera()
-    -- Background
+    -- background
     local sx = love.graphics.getWidth() / sprites.background:getWidth()
     local sy = love.graphics.getHeight() / sprites.background:getHeight()
     love.graphics.draw(sprites.background, 0, 0, nil, sx, sy)
